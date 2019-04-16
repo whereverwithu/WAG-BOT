4f2df7d0cb7d3515493d16afd0fb764705d23f49
whereverwithu <49655286+whereverwithu@users.noreply.github.com>
2019-04-16 22:58:42 +0900

    Update WAGBOT.py

--- WAGBOT.py
+++ WAGBOT.py
@@ -1,11 +1,12 @@
 import discord
+import os
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
-    print("로그인")
+    print("login")
     print(client.user.name)
     print(client.user.id)
     print("------------------")
@@ -17,4 +18,6 @@ async def on_message(message):
     if message.content.startswith("!규칙"):
         await client.send_message(message.channel, "#규칙 에서 규칙을 확인하실 수 있습니다! 규칙이 바뀌면 #공지 에서 공지가 되니 확인해주세요!")
 
-client.run('NTY3NzAzNjYwOTc3NDU1MTMz.XLXaLg.-DTPqW36qnwlmNWUoH812PPy1gk')
+        
+access_token = os.environ["BOT_TOKEN"]        
+client.run('access_token')
