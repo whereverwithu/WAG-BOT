fe17b143e7c296211950f4b9cd21136d19f8f696
whereverwithu <49655286+whereverwithu@users.noreply.github.com>
2019-04-21 01:20:52 +0900

    Update WAGBOT.py

--- WAGBOT.py
+++ WAGBOT.py
@@ -25,14 +25,28 @@ class MyClient(discord.Client):
             await message.channel.send('!안녕'.format(message))
             await message.channel.send('!정보'.format(message))
 
+        if message.content.startswith('!테스터'):
+             await message.channel.send('{0.author.mention}님 테스터에 관한 내용입니다.'.format(message))
+             await message.channel.send('1. 클랜에 들어오신지 **__3주__**가 되셔야합니다.'.format(message))
+             await message.channel.send('2. 채팅을 치시면 레벨이 존재하는데 **__최소 3레벨__**을 달성하셔야합니다.'.format(message))
+             await message.channel.send('3. **__활발하게 활동__**을 하셔야 합격할 확률이 높습니다.'.format(message))
+
         if message.content.startswith('!정보'):
             date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
             embed = discord.Embed(color=0x00ff00)
             embed.add_field(name="이름", value=message.author.name, inline=True)
             embed.add_field(name="서버닉네임", value=message.author.display_name, inline=True)
+            embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일", inline=True)
             embed.add_field(name="아이디   ", value=message.author.id, inline=True)
             embed.set_thumbnail(url=message.author.avatar_url)
             await message.channel.send(message.channel, embed=embed)
+
+        if message.content.startswith('!투표'):
+            vote = message.content[4:].split("/")
+            await message.channel.send(vote[0].format(message))
+            for i in range(1, len(vote)):
+                await message.channel.send(vote[i].format(message))
+                await client.add_reaction(choose, '✔')
 
 
 
