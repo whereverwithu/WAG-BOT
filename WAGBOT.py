2a0b43baf4edb0fa399fe42554b0f4dc69e705cf
whereverwithu <49655286+whereverwithu@users.noreply.github.com>
2019-04-24 00:21:50 +0900

    Update WAGBOT.py

--- WAGBOT.py
+++ WAGBOT.py
@@ -1,54 +1,64 @@
 import discord
-import datetime
+from discord.ext import commands
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+client = commands.Bot(command_prefix="!")
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith('!안녕'):
-            await message.channel.send('안녕하세요. {0.author.mention}'.format(message))
-
-        if message.content.startswith('!규칙'):
-            await message.channel.send('{0.author.mention}님 #규칙 을 이용해주세요. 지키지 않을시 경고를 받습니다.'.format(message))
-
-        if message.content.startswith('!명령어'):
-            await message.channel.send('{0.author.mention}님 여기 명령어 목록 입니다.'.format(message))
-            await message.channel.send('!규칙'.format(message))
-            await message.channel.send('!안녕'.format(message))
-            await message.channel.send('!정보'.format(message))
-
-        if message.content.startswith('!테스터'):
-             await message.channel.send('{0.author.mention}님 테스터에 관한 내용입니다.'.format(message))
-             await message.channel.send('1. 클랜에 들어오신지 **__3주__**가 되셔야합니다.'.format(message))
-             await message.channel.send('2. 채팅을 치시면 레벨이 존재하는데 **__최소 3레벨__**을 달성하셔야합니다.'.format(message))
-             await message.channel.send('3. **__활발하게 활동__**을 하셔야 합격할 확률이 높습니다.'.format(message))
-
-        if message.content.startswith('!정보'):
-            date = datetime.datetime.utcfromtimestamp(((int(message.author.id) >> 22) + 1420070400000) / 1000)
-            embed = discord.Embed(color=0x00ff00)
-            embed.add_field(name="이름", value=message.author.name, inline=True)
-            embed.add_field(name="서버닉네임", value=message.author.display_name, inline=True)
-            embed.add_field(name="가입일", value=str(date.year) + "년" + str(date.month) + "월" + str(date.day) + "일", inline=True)
-            embed.add_field(name="아이디   ", value=message.author.id, inline=True)
-            embed.set_thumbnail(url=message.author.avatar_url)
-            await message.channel.send(message.channel, embed=embed)
-
-        if message.content.startswith('!투표'):
-            vote = message.content[4:].split("/")
-            await message.channel.send(vote[0].format(message))
-            for i in range(1, len(vote)):
-                await message.channel.send(vote[i].format(message))
-                await client.add_reaction(choose, '✔')
+TOKEN = "NTY4NzE0ODAzODI1MjEzNDQw.XL8biw.azg9JwVj2Xl6pqAoXuxrjXiaDh4"
 
 
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print("------")
 
-client = MyClient()
-client.run('NTY3NzIwNjg3MjQ3MTYzMzky.XLXpKQ.QOGeYWw3xyeqAO3s8TwhjBGcw0U')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    user = message.author
+    msg = message.content
+    print(f"{user} said {msg}")
+
+    await client.process_commands(message)
+
+@client.event
+async def on_message_delete(message):
+    await message.channel.send("여기에 메시지가 지워졌습니다.")
+
+@client.command() #!안녕을 치면 안녕하세요가 출력
+async def 안녕(ctx):
+    await ctx.send("안녕하세요!")
+
+@client.command(aliases=["따라하기"]) #내가 보낸 말을 그대로 보냄
+async def say(ctx, *, words):
+    await ctx.send(words)
+
+@client.command()
+async def 위뜨유(ctx):
+    embed = discord.Embed(title="위뜨유의 유튜브", desciption="Description", colour=discord.Color.red(), url="https://www.youtube.com/channel/UC4OEaJ7-78b9XXZdPjns8_A?view_as=subscriber")
+
+    embed.set_author(name=client.user.name, icon_url=client.user.avatar_url)
+    embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url="https://lh3.googleusercontent.com/IR3SH1GToLrOWRQPDabU0RZlv-n1iHQ3kRmdwGbTyJUKAaTxoB6V7j1fOX8Z3suolXi8W7P9S9SI=w220-h220-n-o-rw")
+
+    embed.add_field(name="마우스", value="Logitech G304")
+    embed.add_field(name="감도", value="dpi 1000/ingame 8")
+
+    await ctx.send(embed=embed)
+
+@client.command()
+async def 엘팍(ctx):
+    embed = discord.Embed(title="엘팍이의 유튜브", desciption="Description", colour=discord.Color.red(), url="https://www.youtube.com/channel/UCQMwLUH_C0Qw82cmRQGX7Fw")
+
+    embed.set_author(name=client.user.name, icon_url=client.user.avatar_url)
+    embed.set_footer(text=ctx.author.name, icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url="https://yt3.ggpht.com/a-/AAuE7mDpjK2FInWTXbp369re5Mbj0T9sfX4qZBGbXA=s288-mo-c-c0xffffffff-rj-k-no")
+
+    embed.add_field(name="마우스", value="Logitech G403")
+    embed.add_field(name="감도", value="dpi 1600/ingame 2.25")
+
+    await ctx.send(embed=embed)
+
+
+client.run(TOKEN)
